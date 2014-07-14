94f5be329063f53894f0b7178069f7e68b0e6840
miyamoto masaru <masaringaru@Garu-no-Mac-mini.local>
2014-07-14 14:37:17 +0900

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by miyamoto masaru on 2014/07/14.
+//  Copyright (c) 2014年 com.miyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by miyamoto masaru on 2014/07/14.
+//  Copyright (c) 2014年 com.miyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
